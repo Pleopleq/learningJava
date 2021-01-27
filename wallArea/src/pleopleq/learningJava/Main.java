e901bd48b659a855a96ec6cc9de06a608f043ba6
Felix Anducho <felix.anducho74@gmail.com>
2021-01-26 19:33:07 -0600

    Add new projects

---
+++ wallArea/src/pleopleq/learningJava/Main.java
@@ -0,0 +1,9 @@
+package pleopleq.learningJava;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Wall newWall = new Wall(-1.5, -1.5);
+        newWall.setWidth(-1.25);
+    }
+}
